c651873364614b2b1677743c83c21cf43dcaddb6
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-06-29 11:43:59 -0700

    feat: Build lua files via nix

--- flake.nix
+++ flake.nix
@@ -14,19 +14,34 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        ccPrograms = pkgs.stdenv.mkDerivation {
+          name = "powah-ae2";
+          src = pkgs.lib.cleanSource ./.;
+          nativeBuildInputs = with pkgs; [fennel];
+          buildPhase = ''
+            find . -name "*.fnl" -type f | while read -r fnl_file; do
+              lua_file="''${fnl_file%.fnl}.lua"
+              echo "Compiling $fnl_file -> $lua_file"
+              fennel --compile "$fnl_file" > "$lua_file"
+            done
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp *.lua $out/ || true
+          '';
+        };
       in rec
       {
-        # checks = {};
-        # packages = {};
-        # apps.default = flake-utils.lib.mkApp {
-        #   drv = self.packages.default;
-        # };
+        packages = {
+          ccPrograms = ccPrograms;
+        };
         devShells = {
           default = pkgs.mkShell {
-            # checks = self.checks.${system};
             packages = with pkgs; [
               lua52Packages.readline
               fennel
+              fnlfmt
             ];
           };
         };
